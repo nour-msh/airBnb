20e97d3491a7b984a7f67fca01dd48c7d1ecab0a
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-02 23:42:12 +0300

    Connecting mongoose

--- airBnb-BE/Database/database.js
+++ airBnb-BE/Database/database.js
@@ -1,3 +1,10 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+dotenv.config()
 
+mongoose
+  .connect(
+    process.env.MONGO_URL
+  )
+  .then((result) => console.log("Connection Successful!"))
+  .catch((err) => console.log(err));
