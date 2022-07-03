cab4f2b6193f9c030165cdae78f9d2d0c4b6530f
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-04 01:55:45 +0300

    Styling LoginForm

--- airbnb-frontend/src/Component/LoginForm.js
+++ airbnb-frontend/src/Component/LoginForm.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 
 function LoginForm(){
     return (
-        <div className="container">
+        <div className="loginContainer">
           <form className="myform login">
             <h5>Log In</h5>
     
-            <label>Email:</label>
+            
             <input
               placeholder="Enter Email"
             />
-            <label>Password:</label>
+            
             <input
               placeholder="Enter Password"
               type="password"
             />
-            <button>
+            <button className='login-btn'>
               Login
             </button>
           </form>
--- airbnb-frontend/src/index.js
+++ airbnb-frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './Styles/NavBar.css';
 import './Styles/Dropdown.css';
 import './Styles/Categories.css';
+import './Styles/LoginForm.css';
 import App from './App';
 import './index.css'
 
