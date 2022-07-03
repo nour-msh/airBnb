bd70558559072aa608a65380878a24ef67d2f1df
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-03 19:19:37 +0300

    Adding NavBar component

---
+++ airbnb-frontend/src/Component/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from "../assets/logo.png"
+
+
+function NavBar(){
+    return(
+        <>
+        <img src={logo} alt=""/>
+
+        </>
+    )
+}
+
+export default NavBar;
