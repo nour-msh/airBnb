31003b0b63115a183bbfbfc551bfe531550bd1c3
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-03 20:13:04 +0300

    Adding NavBar buttons
    Adding NavBar dropdown box

--- airbnb-frontend/src/Component/NavBar.js
+++ airbnb-frontend/src/Component/NavBar.js
@@ -4,10 +4,29 @@ import logo from "../assets/logo.png"
 
 function NavBar(){
     return(
-        <>
-        <img src={logo} alt=""/>
+        <div className='navContainer'>
+            <img src={logo} alt=""/>
+            <div className='top-container'>
+                <button>Anywhere |</button>
+                <button>Any Week |</button>
+                <button>Add guests</button>
+                <button></button>
+            </div>
+            <div>
+                <button>Become a Host</button>
+                <button></button>
+                <div>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Sign up</a>
+                        <a class="dropdown-item" href="#">Log in</a>
+                        <a class="dropdown-item" href="#">Host your home</a>
+                        <a class="dropdown-item" href="#">Host an experience</a>
+                        <a class="dropdown-item" href="#">Help</a>
+                    </div>
+                </div>
+            </div>
 
-        </>
+        </div>
     )
 }
 
